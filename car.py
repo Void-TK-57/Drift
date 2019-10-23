10db695577bc4744d944b302096c8bfbfcaa2a12
Void <pedroaa57@gmail.com>
2019-10-23 01:10:14 -0300

    Implemented car moviment

--- car.py
+++ car.py
@@ -7,16 +7,19 @@ from pymunk.vec2d import Vec2d
 # car class
 class Car:
 
-    def __init__(self, x, y, width, height, velocity, color = (255, 153, 187)):
+    def __init__(self, x, y, width, height, velocity, angular_velocity, color = (255, 153, 187)):
         # create body
         self.body = pymunk.Body(mass = 0, moment = 0, body_type= pymunk.Body.DYNAMIC)
         self.body.position = x, y
         #self.shape = pymunk.Poly(self.body, [[0, 0], [0, height], [width, 0], [width, height] ] )
         self.shape = pymunk.Poly.create_box(self.body, (width, height))
-        self.shape.mass = 1
+        self.shape.mass = 0.001
         self.shape.density = 1
         self.fixed_velocity = velocity
         self.color = color
+        self.height = height
+        self.width = width
+        self.fixed_angular_velocity = angular_velocity
 
     # draw the rectangle
     def draw(self, screen):
@@ -28,29 +31,79 @@ class Car:
         pygame.draw.polygon(screen, self.color,  coords)
         # calculate y and x of vertices
         # draw orientation
-        pygame.draw.line(screen, (255, 255, 255), list(self.body.position), self.body.local_to_world( [100*np.sin(self.body.angle) , -100*np.cos(self.body.angle)] ) )
+        pygame.draw.line(screen, (255, 255, 255), list(self.body.position), self.body.local_to_world( [100*np.sin(self.body.angle/180) , -100*np.cos(self.body.angle/180)] ) )
 
     # method to get the local point of the vector
-    def get_point_from_wheel(wheel):
+    def get_point_from_wheel(self, wheel):
         wheels = wheel.split("-")
-        y_orientation = -1 if wheel[0] == "bottom" else 1
-        x_orientation = -1 if wheel[1] == "right"  else 1
-        y = y_orientation * self.shape.height/2
-        x = x_orientation * self.shape.width/2
+        if wheels[0] == "bottom":
+            y_orientation = -1
+        elif wheels[0] == "top":
+            y_orientation = 1
+        else:
+            y_orientation = 0
+        
+        if wheels[0] == "left":
+            x_orientation = -1
+        elif wheels[0] == "right":
+            x_orientation = 1
+        else:
+            x_orientation = 0
+
+        y = y_orientation * self.height/2
+        x = x_orientation * self.width/2
         return x, y
 
+    def polar_to_cartesian(self, angle, length):
+        return [length * np.cos(angle + np.pi/2), -length * np.sin(angle + np.pi/2)]
+
     def apply_force_at_local_point(self, force, wheel):
-        self.body.apply_force_at_local_point(force, wheel)
+        self.body.apply_force_at_local_point(force, self.get_point_from_wheel(wheel) )
 
     def apply_force_at_world_point(self, force, wheel):
-        self.body.apply_force_at_local_point(force, wheel)
+        self.body.apply_force_at_local_point(force, self.get_point_from_wheel(wheel) )
 
     def apply_impulse_at_local_point(self, force, wheel):
-        self.body.apply_force_at_local_point(force, wheel)
+        self.body.apply_force_at_local_point(force, self.get_point_from_wheel(wheel) )
 
     def apply_impulse_at_world_point(self, force, wheel):
-        self.body.apply_force_at_local_point(force, wheel)
-        
+        self.body.apply_force_at_local_point(force, self.get_point_from_wheel(wheel) )
+
+    def move(self, backwards = False):
+        velocity = self.fixed_velocity
+        if backwards:
+            velocity *= -1
+        self.body.velocity = self.polar_to_cartesian(-self.body.angle, velocity)
+
+    def rotate(self, anti = False):
+        angular_velocity = self.fixed_angular_velocity
+        if anti:
+            angular_velocity *= -1
+        self.body.angular_velocity = angular_velocity
+
+    def stop(self, rotation = False):
+        if rotation:
+            self.body.angular_velocity = 0
+        else:
+            self.body.velocity = 0, 0
+
+    
+
+    def log(self):
+        print("Position:")
+        print(self.body.position)
+        print("Velocity:")
+        print(self.body.velocity)
+        print("Force:")
+        print(self.body.force)
+        print("-"*5)
+        print("Angle:")
+        print(self.body.angle/np.pi % 2)
+        print("Angular Velocity:")
+        print(self.body.angular_velocity)
+        print("Torque:")
+        print(self.body.torque)
+        print("="*20)
 
     # update velocity
     def update(self):
--- main.py
+++ main.py
@@ -9,9 +9,6 @@ import pymunk
 def update_game(screen, space, car, _map, dt):
     # update worlrd
     space.step(dt/1000)
-    #car.body.torque = 4
-    print( car.body.position )
-    print( car.body.angle)
     # reset screen and redraw
     screen.fill((0, 0, 0))
     car.draw(screen)
@@ -19,49 +16,69 @@ def update_game(screen, space, car, _map, dt):
     pygame.display.update()
 
 def main():
-
+    # init game
     pygame.init()
 
     # display height and width
     display_height = 600
-    disply_width = 1000
+    disply_width = 800
+    # UP, RIGHT, DOWN, LEFT
+    key_pressed = [False, False, False, False]
+
 
     screen = pygame.display.set_mode( (disply_width, display_height) )
     pygame.display.set_caption('Race Drift')
 
     clock = pygame.time.Clock()
-    # check if the game is still alive
-    alive = True
-
     _map = []
     
     space = pymunk.Space()
     space.gravity = 0, 0
 
-    car = Car(x = 200, y = 200, width = 14, height = 32, velocity = 10)
+    car = Car(x = 200, y = 200, width = 14, height = 32, velocity = 200, angular_velocity = 1.4)
 
     space.add(car.body, car.shape)
 
-    
     # main loop
-    while alive:
+    while True:
         # for each event
         for event in pygame.event.get():
             # if event is quit, then set alive to false
             if event.type == pygame.QUIT:
-                alive = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_DOWN:
-                    # add for at the bottom of the car
-                    print("Up")
-                    
-                elif event.key == pygame.K_UP:
-                    print("Down")
+                pygame.quit()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
+                # change state of the key pressed
+                if event.key ==pygame.K_UP:
+                    key_pressed[0] = not key_pressed[0]
+                if event.key ==pygame.K_RIGHT:
+                    key_pressed[1] = not key_pressed[1]
+                if event.key ==pygame.K_DOWN:
+                    key_pressed[2] = not key_pressed[2]
+                if event.key ==pygame.K_LEFT:
+                    key_pressed[3] = not key_pressed[3]
 
+        
+        # if there is a key pressed, check which one
+        if key_pressed[0]:
+            car.move()
+        if key_pressed[2]:
+            car.move(backwards=True)
+        if key_pressed[1]:
+            car.rotate()
+        if key_pressed[3]:
+            car.rotate(anti = True)
+
+        # if neither of keys are pressed, stop car
+        if not ( key_pressed[0] or key_pressed[2] ):
+            car.stop()
+        if not ( key_pressed[1] or key_pressed[3] ):
+            car.stop(True)
     
         # update game and tick clock
         update_game(screen, space, car, _map, clock.get_time())
         clock.tick(60)
+
+        car.log()
         
     
     # quit
